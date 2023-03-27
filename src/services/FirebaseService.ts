f2f3f6929d04009fc28a5a07a360cb55e85ae602
gw98-github <grzegorz.wazny.pv@gmail.com>
2023-03-27 20:10:26 +0200

    firebase config

--- src/services/FirebaseService.ts
+++ src/services/FirebaseService.ts
@@ -4,13 +4,7 @@ import { useDatabaseObject } from 'vuefire'
 // ... other firebase imports
 
 const firebaseApp = initializeApp({
-  apiKey: 'AIzaSyAa83RgfJFMYPyEdXBsDYQ0raIB6rjDjvA',
-  authDomain: 'shop-e919e.firebaseapp.com',
-  databaseURL: 'https://shop-e919e-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'shop-e919e',
-  storageBucket: 'shop-e919e.appspot.com',
-  messagingSenderId: '388988547964',
-  appId: '1:388988547964:web:28c90b8803a8a08770aaf9'
+  // paste firebase config
 })
 
 // used for the databas refs
